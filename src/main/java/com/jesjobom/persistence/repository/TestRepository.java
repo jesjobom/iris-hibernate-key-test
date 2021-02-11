043813cc3b9ff2199dd05052483d7ebcc4e312e3
Jairton Junior <jairton.savicki@bbts.com.br>
2021-02-11 11:12:20 -0300

    Simple id

--- src/main/java/com/jesjobom/persistence/repository/TestRepository.java
+++ src/main/java/com/jesjobom/persistence/repository/TestRepository.java
@@ -1,8 +1,7 @@
 package com.jesjobom.persistence.repository;
 
 import com.jesjobom.persistence.model.Test;
-import com.jesjobom.persistence.model.TestPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TestRepository extends JpaRepository<Test, TestPK> {
+public interface TestRepository extends JpaRepository<Test, Long> {
 }
